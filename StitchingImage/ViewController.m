243d06aad0e845432ef25382a26fcfc7b457d2cc
zhengjinghua <jin114001251@gmail.com>
2015-10-26 20:43:21 +0800

    Init project

--- StitchingImage/ViewController.m
+++ StitchingImage/ViewController.m
@@ -7,21 +7,68 @@
 //
 
 #import "ViewController.h"
+#import "StitchImage.h"
+
+#define DeviceWidth [UIScreen mainScreen].bounds.size.width
+#define DeviceHeight [UIScreen mainScreen].bounds.size.height
+
+const CGFloat canvasViewSideLength = 100.0f;
 
 @interface ViewController ()
-
+{
+    CGFloat _beginningY;
+    CGFloat _canvasViewMargin;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"example";
+    
+    _canvasViewMargin = (DeviceWidth - canvasViewSideLength * 3) / 4;
+    _beginningY = (DeviceHeight - canvasViewSideLength * 3 - _canvasViewMargin * 2) / 2;
+    
+    [self createCanvasViews];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)createCanvasViews {
+    int maxRow = 3;
+    int maxColumn = 3;
+    
+    for (int i = 0; i < 9; i++) {
+        int row = floor((float)i / maxRow);
+        int column = i % maxColumn;
+        
+        CGFloat originX = _canvasViewMargin + column * canvasViewSideLength + column * _canvasViewMargin;
+        CGFloat originY = _beginningY + row * canvasViewSideLength + row * _canvasViewMargin;
+        
+        UIImageView *canvasView = [[UIImageView alloc] initWithFrame:CGRectMake(originX, originY, canvasViewSideLength, canvasViewSideLength)];
+        canvasView.layer.cornerRadius = canvasViewSideLength / 10;
+        canvasView.layer.masksToBounds = YES;
+        canvasView.backgroundColor = [UIColor colorWithWhite:0.839 alpha:1.000];
+        
+        int imageViewsCount = i + 1;
+        UIImageView *exampleView = [self createImageViewWithCanvasView:canvasView withImageViewsCount:imageViewsCount];
+        
+        [self.view addSubview:exampleView];
+    }
+}
+
+- (UIImageView *)createImageViewWithCanvasView:(UIImageView *)canvasView withImageViewsCount:(NSInteger)count {
+    NSMutableArray *imageViews = [[NSMutableArray alloc] init];
+    
+    for (int index = 1; index <= count; index++) {
+        UIImageView *imageView = [UIImageView new];
+        
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d", index]];
+        imageView.image = image;
+        
+        [imageViews addObject:imageView];
+    }
+    
+    return [[StitchImage alloc] stitchingOnImageView:canvasView withImageViews:imageViews];
 }
 
 @end
